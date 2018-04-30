3a231e4fe70e29b7143d7ab5d92f22423ed7a621
min_2271 <min_2271@naver.com>
2018-04-30 13:28:12 +0900

    프로젝트 구조 변경

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     # GET 'home/'
     path('', views.IndexView.as_view(), name='index'),
 
+    # GET 'home/main/'
+    path('main/', views.MainView.as_view(), name='main'),
+    
     # GET 'home/nanjang'
     path('nanjang/', views.NanjangListView.as_view(), name='nanjang'),
     
--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 
 class IndexView(generic.TemplateView):
-    template_name ='home/index.html'
+    template_name = 'home/index.html'
+
+class MainView(generic.TemplateView):
+    template_name = 'home/main.html'
 
 class NanjangListView(generic.ListView):
     pass
