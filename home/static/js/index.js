094974775ec6e97e4688201c74829c97fe528532
min_2271 <min_2271@naver.com>
2018-05-04 03:49:01 +0900

    href 경로 수정

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -13,6 +13,7 @@ window.onload = function() {
 }
 
 function movePage(target) {
-    window.location.href = "http://wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
+    
+    window.location.href = "wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
     // window.location.href = "http://www.localhost:8000/home/main/#" + target;
 }
