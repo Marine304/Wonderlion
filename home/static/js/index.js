2f6509a2974026c87f579f8ddd14d37339f86f57
min_2271 <min_2271@naver.com>
2018-05-04 03:42:36 +0900

    좋아요 기능 에러 수정

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -14,4 +14,5 @@ window.onload = function() {
 
 function movePage(target) {
     window.location.href = "http://wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
+    // window.location.href = "http://www.localhost:8000/home/main/#" + target;
 }
