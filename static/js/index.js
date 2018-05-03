7476e01295145ed3a721c25ee33c0b35a0e7bab4
min_2271 <min_2271@naver.com>
2018-05-04 03:58:55 +0900

    Collect static

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
-window.onload=function() {
+window.onload = function() {
     var wonderful = document.getElementById("main_nav");
-    console.log(wonderful);
+
     if (wonderful) {
       wonderful.addEventListener("mouseover", function(event) {
           event.target.style.color = "#e85757";
@@ -9,4 +9,11 @@ window.onload=function() {
           }, 500);
         }, false);
     }
+
+}
+
+function movePage(target) {
+    
+    window.location.href = "http://wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
+    // window.location.href = "http://www.localhost:8000/home/main/#" + target;
 }
