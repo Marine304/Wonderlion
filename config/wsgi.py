46c065afeff040988d1b726ad6fccadf4c7a7d6a
min_2271 <min_2271@naver.com>
2018-05-04 03:07:58 +0900

    구조변경

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,12 +1,16 @@
+"""
+WSGI config for wonderful project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
-sys.path.append(os.path.join(app_path, 'wonderful'))
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wonderful.settings")
 
 application = get_wsgi_application()
