29eacbb66f0d1bbe6e9dadc00f1de43ac37984e1
min_2271 <min_2271@naver.com>
2018-05-04 03:52:12 +0900

    경로지정 수정

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -14,6 +14,6 @@ window.onload = function() {
 
 function movePage(target) {
     
-    window.location.href = "wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
+    window.location.href = "http://wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
     // window.location.href = "http://www.localhost:8000/home/main/#" + target;
 }
