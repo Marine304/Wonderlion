b6c7c01fef7447145fef1700729969eea5eeb68b
min_2271 <min_2271@naver.com>
2018-05-04 04:16:47 +0900

    Collect statics

--- static/js/index.js
+++ static/js/index.js
@@ -12,8 +12,7 @@ window.onload = function() {
 
 }
 
-function movePage(target) {
-    
+function movePage(target) { 
     window.location.href = "http://wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/#" + target;
     // window.location.href = "http://www.localhost:8000/home/main/#" + target;
 }
