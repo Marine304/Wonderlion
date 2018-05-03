14b87e56aa9de19355b6b0075d3e5410cbe90ba0
min_2271 <min_2271@naver.com>
2018-05-04 04:52:53 +0900

    난장 리스트 태그 표시 및 에러 수정

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,9 @@
 window.onload = function() {
-    var wonderful = document.getElementById("main_nav");
+    var wonderful = document.getElementById("wonderful");
 
     if (wonderful) {
-      wonderful.addEventListener("mouseover", function(event) {
-          event.target.style.color = "#e85757";
-          setTimeout(function() {
-            event.target.style.color = "white";
-          }, 500);
+      wonderful.addEventListener("click", function(event) {
+          window.location.href = "http://wonderful.ap-northeast-2.elasticbeanstalk.com/home/main/";
         }, false);
     }
 
