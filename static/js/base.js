77998ef52a92de148e5de83cbfa8ff211b33ac39
min_2271 <min_2271@naver.com>
2018-05-05 15:09:10 +0900

    구글 애널리스틱 추가

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 window.onload=function() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    document.querySelectorAll('a[href^="#n_"]').forEach(anchor => {
         anchor.addEventListener("click", function(e) {
             e.preventDefault();
             document.querySelector(this.getAttribute("href")).scrollIntoView({
