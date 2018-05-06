0ccd870a5a7018c94c50eb5aa1651a56bf00d4ad
min_2271 <min_2271@naver.com>
2018-05-06 15:11:27 +0900

    디테일 페이지 이미지 필드 추가

--- jujum/models.py
+++ jujum/models.py
@@ -5,7 +5,12 @@ class Jujum(models.Model):
 
     title = models.CharField(max_length=20)
     content = models.TextField(blank=True, null=True)
+
     menu = models.ImageField(null=True, blank=True, upload_to='home/%Y/%m/%d')
+    image1 = models.ImageField(null=True, blank=True, upload_to='home/%Y/%m/%d')
+    image2 = models.ImageField(null=True, blank=True, upload_to='home/%Y/%m/%d')
+    image3 = models.ImageField(null=True, blank=True, upload_to='home/%Y/%m/%d')
+
     tags = TaggableManager()
     like_count = models.IntegerField(default=0)
     lat = models.DecimalField(max_digits=9, decimal_places=6, null=True)
